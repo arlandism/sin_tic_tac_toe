911a19ef5711285603acef278fa62de8d3c26c6e
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-07-23 18:24:04 -0500

    more tests

--- lib/client.rb
+++ lib/client.rb
@@ -1,8 +1,14 @@
 
 class ClientSocket
   attr_reader :port
+  attr_reader :connected
 
   def initialize(port=5000)
     @port = port
+    @connected = false
+  end
+
+  def connect(socket)
+    @connected = true
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -6,8 +6,24 @@ describe 'ClientSocket' do
   end
 
   describe "initialization" do
-   it "sets a default port" do
-     @socket.port.should be 5000
-   end 
+    it "sets a default port" do
+      @socket.port.should be 5000
+    end 
+
+    it "sets connected var to false" do
+      @socket.connected.should be false
+    end
+
+    it "allows a port to be set" do
+      socket = ClientSocket.new 3000
+      socket.port.should be 3000
+    end
+  end
+
+  describe "connect" do
+    it "connects to another socket" do
+      @socket.connect("").should be true
+    end
   end
 end
+
