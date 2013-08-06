dc75540842f938bba971d5dc1a4cc5fd1ea29358
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-06 09:55:46 -0500

    Add second @transmitter receive to grab winner

--- lib/ai.rb
+++ lib/ai.rb
@@ -3,6 +3,8 @@ require_relative 'json_transmitter'
 
 class AI
   
+  attr_reader :winner
+
   def initialize
     socket = ClientSocket.new 
     socket.connect!
@@ -10,8 +12,11 @@ class AI
   end
 
   def next_move(board_state)
+     updated_game_state = Hash.new
      @transmitter.send(board_state)
-     @transmitter.receive
+     updated_game_state["move"] = @transmitter.receive
+     updated_game_state["winner"] = @transmitter.receive
+     return updated_game_state
   end
 
 end
