f9c3605cc11f69357a9af9b31fa4f5c2d1e87183
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-06 09:57:45 -0500

    Add set winner cookie if exists plus helper functions

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -13,11 +13,36 @@ describe 'main' do
      get '/'
      last_response.status.should be 200
      end
+
+    context "x is the winner"
+    it "shows a win message if there's winner" do
+      set_cookie("winner=x")
+      get '/'
+      last_response.status.should be 200
+      last_response.body.should include "x wins"
+    end
+
+    context "o is the winner"
+    it "shows a win message if there's winner" do
+      set_cookie("winner=o")
+      get '/'
+      last_response.status.should be 200
+      last_response.body.should include "o wins"
+    end
+
+    it "shouldn't show a win message if there's no winner" do
+      get '/'
+      last_response.body.should_not include "x wins"
+    end
   end
 
   describe "POST '/move'" do
+
+    default_move = {"move" => 3}
     
     it "redirects to index" do
+      AI.any_instance.stub(:new)
+      AI.any_instance.stub(:next_move).and_return(default_move)
       execute_post_request_with_move 5
       follow_redirect!
       default_url = "http://example.org"
@@ -25,25 +50,27 @@ describe 'main' do
       last_request.url.should == default_url + expected_path
     end
 
-
     it "stores player move cookie " do
+      AI.any_instance.stub(:new)
+      AI.any_instance.stub(:next_move).and_return(default_move)
       execute_post_request_with_move 6
       verify_cookie_value(6,"x")
     end
 
     it "stores AI move cookie" do
-      AI.any_instance.stub(:next_move).and_return(12)
+      AI.any_instance.stub(:next_move).and_return(default_move)
       execute_post_request_with_move 6
-      verify_cookie_value(12,"o")
+      verify_cookie_value(3,"o")
     end
 
     it "stores AI move again" do
-      AI.any_instance.stub(:next_move).and_return(82)
+      AI.any_instance.stub(:next_move).and_return({"move" => 82})
       execute_post_request_with_move 6
       verify_cookie_value(82,"o")
     end
 
     it "has cookies that are persistent across requests" do
+      AI.any_instance.stub(:next_move).and_return(default_move)
       execute_post_request_with_move 6
       get '/'
       verify_cookie_value(6,"x")
@@ -60,7 +87,7 @@ describe 'main' do
     it "has cookies that are persistent across multiple post requests" do
       ai = mock(:AI)
       AI.stub(:new).and_return(ai)
-      ai.stub(:next_move).and_return(6)
+      ai.stub(:next_move).and_return({"move" => 6})
       execute_post_request_with_move 8
       execute_post_request_with_move 5
       verify_cookie_value(8,"x")
@@ -72,13 +99,22 @@ describe 'main' do
     it "gives AI.next current cookies" do
       ai = mock(:AI)
       AI.stub(:new).and_return(ai)
-      ai.stub(:next_move).and_return(5)
+      ai.stub(:next_move).and_return({"move" => 5})
       execute_post_request_with_move 6
       verify_cookie_value(6,"x")
       verify_cookie_value(5,"o")
       current_board_state = {5 => "o", 6 => "x", 7 => "x"}
       ai.should_receive(:next_move).with(current_board_state)
       execute_post_request_with_move 7
+    end
+
+    it "gets the winner from AI" do
+      game_stuff = {"move" => 3, "winner" => "x"}
+      ai = mock(:AI)
+      AI.stub(:new).and_return(ai)
+      ai.stub(:next_move).and_return(game_stuff)
+      execute_post_request_with_move 2
+      rack_mock_session.cookie_jar["winner"].should == "x"
     end
 
     def execute_post_request_with_move(move)
@@ -99,4 +135,27 @@ describe 'main' do
       rack_mock_session.clear_cookies
     end
   end
+
+  describe "#winner" do
+    context "o is the winner"
+    it "pulls winner out of a hash" do
+      winner({"winner" => "o"}).should == "o"
+    end
+
+    context "x is the winner"
+    it "pulls winner out of a hash" do
+      winner({"winner" => "x"}).should == "x"
+    end
+
+    context "winner is nil"
+    it "pulls winner out of hash" do
+      winner({"winner" => nil}).should == nil
+    end
+  end
+
+  #describe "integration" 
+  #  it "gets a move from AI and stores it as a cookie" do
+  #    post '/move', {:player_move => 3}   
+  #    rack_mock_session.cookie_jar["3"].should == "x"
+  #  end
 end
