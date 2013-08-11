850f7368b43042d7b53779b353b53f89bf593773
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-11 10:58:13 -0500

    Adds tests for winner presenter

---
+++ spec/winner_presenter_spec.rb
@@ -0,0 +1,22 @@
+require 'winner_presenter'
+
+describe "#Winner" do
+
+  describe "#render_if_winner" do
+    it "doesn't show anything if there's no winner" do
+      Winner.render_if_winner({}).should == nil
+    end
+
+    it "shows x if x is the winner" do
+      Winner.render_if_winner({"winner" => "x"}).should == "x wins"
+    end
+
+    it "shows o if o is the winner" do
+      Winner.render_if_winner({"winner" => "o"}).should == "o wins"
+    end
+
+    it "shows nothing if winner is empty space" do
+      Winner.render_if_winner({"winner" => ""}).should == nil
+    end
+  end
+end
