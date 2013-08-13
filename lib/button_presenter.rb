0f4289d40fe4207a5556313a92f5ddc3405fe8d5
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-13 15:10:54 -0500

    Adds blanking of buttons at end of game

--- lib/button_presenter.rb
+++ lib/button_presenter.rb
@@ -1,19 +1,23 @@
 
 class ButtonPresenter
 
-  def self.render(move_hash,number_of_buttons)
+  def self.render(board_state,number_of_buttons)
     buttons = Array.new
     button_template = "<input type='submit', value='%s', name='player_move'>"
     filled_button_template = "<input type='submit', value='%s', name='player_move', disabled>"
 
-    (1..number_of_buttons).each do |num|
-      button = move_hash[num.to_s] ? filled_button_template % move_hash[num.to_s]: button_template % num
-      buttons << button
-      if num % 3 == 0
-        buttons << "<div></div>" 
-      end
-    end
+      (1..number_of_buttons).each do |num|
+        if board_state["winner"] == "" or board_state["winner"].nil?
+          button = board_state[num.to_s] ? filled_button_template % board_state[num.to_s]: button_template % num
+        else
+          button = board_state[num.to_s] ? filled_button_template % board_state[num.to_s]: filled_button_template % num
+        end
+        buttons << button
 
+        if num % 3 == 0
+          buttons << "<div></div>" 
+        end
+      end
     buttons.join("") 
   end
 end
