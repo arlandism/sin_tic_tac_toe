3d17eb0227d18d84d035805c8123b9a7142dc2f0
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-14 15:27:30 -0500

    Revert "Revert "Adds tests for blanking out of buttons at end of game""
    This reverts commit 3f3195829896f76a962de4d7d850166f216bb648.

--- spec/button_presenter_spec.rb
+++ spec/button_presenter_spec.rb
@@ -5,6 +5,12 @@ describe ButtonPresenter do
   describe "#render" do
 
     button_template = "<input type='submit', value='%s', name='player_move'>"
+    
+    blanked_button_template = "<input type='submit', value='%s', name='player_move', disabled>"
+
+    three_button_layout = [ "<input type='submit', value='1', name='player_move'>",
+                            "<input type='submit', value='2', name='player_move'>",
+                            "<input type='submit', value='3', name='player_move'>"].join("") + "<div></div>"
 
     def render_returns_expected?(hash,num_buttons)
       ButtonPresenter.render(hash,num_buttons).should == @buttons.join("") + "<div></div>" 
@@ -14,12 +20,10 @@ describe ButtonPresenter do
       @buttons = Array.new
     end
 
-    context "given a blank hash with no values"
-    it "outputs 3 buttons with numeric values" do
-      (1..3).each do |num|
-        @buttons << button_template % num 
+    context "given a blank hash with no values" do
+      it "outputs 3 buttons with numeric values" do
+        ButtonPresenter.render({"winner" => ""},3).should == three_button_layout 
       end
-      render_returns_expected?({},3)
     end
 
     it "outputs 4 buttons with numeric values" do
@@ -29,29 +33,47 @@ describe ButtonPresenter do
           @buttons << "<div></div>"
         end
       end
-      ButtonPresenter.render({},4).should == @buttons.join("")
+      ButtonPresenter.render({"winner" => ""},4).should == @buttons.join("")
     end
 
-    context "given a hash with values present"
-    it "outputs 3 disabled buttons with values" do 
-      button_filled = "<input type='submit', value='%s', name='player_move', disabled>"
-      ["x","o","x"].each do |token|
-        @buttons << button_filled % token
-      end
-      @buttons << "<div></div>"
-      board = {"1" => "x", "2" => "o", "3" => "x"}
-      ButtonPresenter.render(board,3).should == @buttons.join("")
-    end
-
-    context "given number_of_buttons = 9"
-    it "outputs a div after 3 buttons" do
-      (1..9).each do |num|
-        @buttons << button_template % num
-        if num % 3 == 0
-          @buttons << "<div></div>"
+    context "given a hash with values present" do
+      it "outputs 3 disabled buttons with values" do 
+        button_filled = "<input type='submit', value='%s', name='player_move', disabled>"
+        ["x","o","x"].each do |token|
+          @buttons << button_filled % token
         end
+        @buttons << "<div></div>"
+        board = {"1" => "x", "2" => "o", "3" => "x"}
+        ButtonPresenter.render(board,3).should == @buttons.join("")
       end
-      ButtonPresenter.render({},9).should == @buttons.join("") 
+    end
+
+    context "given number_of_buttons = 9" do
+      it "outputs a div after 3 buttons" do
+        (1..9).each do |num|
+          @buttons << button_template % num
+          if num % 3 == 0
+            @buttons << "<div></div>"
+          end
+        end
+        ButtonPresenter.render({"winner" => ""},9).should == @buttons.join("") 
+      end
+    end
+
+    context "given board with empty winner" do
+      it "outputs normal board" do
+        ButtonPresenter.render({"winner" => ""},3).should == three_button_layout
+      end
+    end
+    
+    context "given board with non-nil winner" do
+      it "outputs board with blanked-out keys" do
+        buttons = Array.new
+        blanked_buttons = (1..3).each do |n|
+          buttons << blanked_button_template % n 
+        end 
+        ButtonPresenter.render({"winner" => "x"},3).should == buttons.join("") + "<div></div>" 
+      end
     end
   end
 end      
