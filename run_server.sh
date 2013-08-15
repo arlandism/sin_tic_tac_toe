b9645351dbb74775d3295909888d079b07eee324
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-15 14:36:34 -0500

    Changes name of bash script

---
+++ run_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python /Users/arlandislawrence/development/python/tic_tac_toe/network_io/start_server.py &>log.txt 
--- run_server_and_sinatra.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python /Users/arlandislawrence/development/python/tic_tac_toe/network_io/start_server.py &
-ruby main.rb
