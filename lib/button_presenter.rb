049fea4b25aaa117bdf435184986e0ff1538cbe9
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-15 14:38:50 -0500

    Refactors button presenter

--- lib/button_presenter.rb
+++ lib/button_presenter.rb
@@ -3,14 +3,14 @@ class ButtonPresenter
 
   def self.render(board_state,number_of_buttons)
     buttons = Array.new
-    button_template = "<input type='submit', value='%s', name='player_move'>"
-    filled_button_template = "<input type='submit', value='%s', name='player_move', disabled>"
-
+    base_button = "<input type='submit', value='%s', name='player_move', id='%s'>"
+    after_id_tag = 61
+    filled_base_button = String.new(base_button).insert(after_id_tag, ", disabled") 
       (1..number_of_buttons).each do |num|
         if board_state["winner"] == "" or board_state["winner"].nil?
-          button = board_state[num.to_s] ? filled_button_template % board_state[num.to_s]: button_template % num
+          button = board_state[num.to_s] ? filled_base_button % [board_state[num.to_s],num]: base_button % [num,num]
         else
-          button = board_state[num.to_s] ? filled_button_template % board_state[num.to_s]: filled_button_template % num
+          button = board_state[num.to_s] ? filled_base_button % [board_state[num.to_s],num]: filled_base_button % [num,num]
         end
         buttons << button
 
--- spec/button_presenter_spec.rb
+++ spec/button_presenter_spec.rb
@@ -4,13 +4,13 @@ describe ButtonPresenter do
 
   describe "#render" do
 
-    button_template = "<input type='submit', value='%s', name='player_move'>"
+    button_template = "<input type='submit', value='%s', name='player_move', id='%s'>"
     
-    blanked_button_template = "<input type='submit', value='%s', name='player_move', disabled>"
+    blanked_button_template = String.new(button_template).insert(61,", disabled") 
 
-    three_button_layout = [ "<input type='submit', value='1', name='player_move'>",
-                            "<input type='submit', value='2', name='player_move'>",
-                            "<input type='submit', value='3', name='player_move'>"].join("") + "<div></div>"
+    three_button_layout = [ "<input type='submit', value='1', name='player_move', id='1'>",
+                            "<input type='submit', value='2', name='player_move', id='2'>",
+                            "<input type='submit', value='3', name='player_move', id='3'>"].join("") + "<div></div>"
 
     def render_returns_expected?(hash,num_buttons)
       ButtonPresenter.render(hash,num_buttons).should == @buttons.join("") + "<div></div>" 
@@ -28,7 +28,7 @@ describe ButtonPresenter do
 
     it "outputs 4 buttons with numeric values" do
       (1..4).each do |num|
-        @buttons << button_template % num
+        @buttons << button_template % [num,num]
         if num == 3
           @buttons << "<div></div>"
         end
@@ -38,11 +38,12 @@ describe ButtonPresenter do
 
     context "given a hash with values present" do
       it "outputs 3 disabled buttons with values" do 
-        button_filled = "<input type='submit', value='%s', name='player_move', disabled>"
-        ["x","o","x"].each do |token|
-          @buttons << button_filled % token
+        button_filled = blanked_button_template
+        ["x","o","x"].each_with_index do |token, index|
+          id_num = index + 1
+          @buttons << button_filled % [token, id_num]
         end
-        @buttons << "<div></div>"
+        @buttons << "<div></div>" 
         board = {"1" => "x", "2" => "o", "3" => "x"}
         ButtonPresenter.render(board,3).should == @buttons.join("")
       end
@@ -51,7 +52,7 @@ describe ButtonPresenter do
     context "given number_of_buttons = 9" do
       it "outputs a div after 3 buttons" do
         (1..9).each do |num|
-          @buttons << button_template % num
+          @buttons << button_template % [num, num]
           if num % 3 == 0
             @buttons << "<div></div>"
           end
@@ -70,7 +71,7 @@ describe ButtonPresenter do
       it "outputs board with blanked-out keys" do
         buttons = Array.new
         blanked_buttons = (1..3).each do |n|
-          buttons << blanked_button_template % n 
+          buttons << blanked_button_template % [n,n] 
         end 
         ButtonPresenter.render({"winner" => "x"},3).should == buttons.join("") + "<div></div>" 
       end
