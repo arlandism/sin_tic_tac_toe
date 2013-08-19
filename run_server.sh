b46b9e2eea3ce2422f7e845d720005e604e5eb95
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-18 20:13:43 -0500

    Runs main

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python /Users/arlandislawrence/development/python/tic_tac_toe/network_io/start_server.py &>log.txt 
+python /Users/arlandislawrence/development/python/tic_tac_toe/network_io/start_server.py &>log.txt &
+ruby main.rb
