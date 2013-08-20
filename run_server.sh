110679c5b541c14792753cfa87469024ddfc86df
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-08-20 09:49:10 -0500

    Added service directory for Python components

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python /Users/arlandislawrence/development/python/tic_tac_toe/network_io/start_server.py &>log.txt &
+python service/network_io/start_server.py &>log.txt &
 ruby main.rb
