154bec42d60519dc1aa40697b34ff8329060e5e6
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-07 20:34:07 -0500

    Extracts method

--- lib/game_information.rb
+++ lib/game_information.rb
@@ -12,11 +12,20 @@ class GameInformation
 
   def service_response
     game_state = {"board" => moves_on_board}
-    game_state["depth"] = @current_game_information["depth"]|| 20
-    AI.new.next_move(game_state)
+    game_state["depth"] = depth
+    what_we_got = AI.new.next_move(game_state)
+    return what_we_got
   end
 
   private
+
+  def depth
+    extracted_depth = @current_game_information["depth"]
+    if extracted_depth == nil or extracted_depth == ""
+      extracted_depth = 20
+    end
+    extracted_depth
+  end
 
   def moves_on_board
     @current_game_information.select{ |key,_| key=~/^[0-9]+$/ }
