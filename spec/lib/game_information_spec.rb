7a65834b74b1d74cd669a8159df6bec003aa834e
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-08 09:38:59 -0500

    Refactors tests

--- spec/lib/game_information_spec.rb
+++ spec/lib/game_information_spec.rb
@@ -16,7 +16,25 @@ describe GameInformation do
 
       GameInformation.new(board_state).service_response 
     end 
+    
+  end
 
+  describe "#winner_on_board" do
+    
+    it "returns the winner that AI gave it" do
+      @ai.stub(:next_move).and_return("winner_on_board" => "me")
+
+      GameInformation.new({}).winner_on_board.should == "me"
+    end
+    
+    it "calls AI if received winner blank or nil" do
+      game_state = {"winner" => nil}
+      @ai.should_receive(:next_move).and_return("winner_on_board" => "x")
+      GameInformation.new(game_state).winner_on_board 
+    end
+  end
+  
+  describe "integration with DefaultStrategy" do
     it "defaults the depth if it can't find it" do
       board_state = {}
       @ai.should_receive(:next_move).with({"board" => {}, "depth" => 20})
@@ -30,27 +48,12 @@ describe GameInformation do
 
       GameInformation.new(game_state).service_response
     end
-  end
 
-  describe "#winner_on_board" do
-    
-    it "returns the winner that AI gave it" do
-      @ai.stub(:next_move).and_return("winner_on_board" => "me")
-
-      GameInformation.new({}).winner_on_board.should == "me"
-    end
-
-    it "sends back the same winner if there's already one stored" do
+   it "sends back the same winner if there's already one stored" do
       game_state = {"winner" => "x"}
       @ai.stub(:next_move).and_return("winner_on_board" => "new winner")
 
       GameInformation.new(game_state).winner_on_board.should == "x"
-    end
-    
-    it "calls AI if received winner blank or nil" do
-      game_state = {"winner" => nil}
-      @ai.should_receive(:next_move).and_return("winner_on_board" => "x")
-      GameInformation.new(game_state).winner_on_board 
     end
   end
 end
