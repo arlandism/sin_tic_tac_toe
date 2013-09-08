c5fbc526558e970b07d2ba4e2fe8133650c71a24
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-07 19:43:56 -0500

    Adds default depth

--- lib/game_information.rb
+++ lib/game_information.rb
@@ -2,21 +2,17 @@ require_relative 'ai'
 
 class GameInformation
 
-  def initialize(move_one, move_two, current_game_information)
-    @move_one = move_one
-    @move_two = move_two
+  def initialize(current_game_information)
     @current_game_information = current_game_information
   end
 
   def winner_on_board
-    game_information = service_response(@move_one,@move_two)
-    winner = game_information["winner_on_board"]
+    winner = service_response["winner_on_board"]
   end
 
-  def service_response(latest_move,other_move)
+  def service_response
     game_state = {"board" => moves_on_board}
-    game_state["board"][latest_move] = "x"
-    game_state["depth"] = @current_game_information[:depth]
+    game_state["depth"] = @current_game_information["depth"]|| 20
     AI.new.next_move(game_state)
   end
 
