47d272639914ddde638e94ebe013ca26121a5a99
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-09 10:41:27 -0500

    Deletes dead code in NextPlayer spec

--- spec/lib/next_player_spec.rb
+++ spec/lib/next_player_spec.rb
@@ -32,21 +32,13 @@ describe NextPlayer do
       NextPlayer.move(board)
     end
 
-    it "returns what AI gives it" do
-      NextPlayer.stub(:is_human?).and_return(false)
-
-      @ai.should_receive(:next_move).and_return({"ai_move" => 17})
-
-      NextPlayer.move({}).should == 17
-   end
-
     it "returns nil for a human move" do
       NextPlayer.stub(:is_human?).and_return(true)
       NextPlayer.move({}).should == nil
     end
 
   end
-end
+
   describe "integration with GameInformation" do
 
     it "delegates to GameInformation for AI response" do
@@ -57,3 +49,4 @@ end
       NextPlayer.move(game_state).should == 3
     end
   end
+end
