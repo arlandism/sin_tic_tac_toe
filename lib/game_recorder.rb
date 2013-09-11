42124d538a682ef66190ab7982674c9467eef53d
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-11 13:36:15 -0500

    Removes is_move? vs is_non_nil_winner? typechecking in GameRecorder

--- lib/game_recorder.rb
+++ lib/game_recorder.rb
@@ -4,10 +4,8 @@ class GameRecorder
 
   def self.write_to_history(to_write,file=File.open("game_history.json","a"))
     record = Record.new(to_write)
-    if record.is_a_move? 
-      file.write(JSON.dump("moves" => [to_write]) + "\n")
-    elsif  record.is_non_nil_winner?
-      file.write(JSON.dump(to_write) + "\n")
+    if  record.is_non_nil_winner?
+      file.write(JSON.dump(to_write))
     end
     file.close
   end
@@ -23,11 +21,6 @@ class Record
 
   def is_non_nil_winner?
     !@content.include?("winner") or @content["winner"] != nil
-  end
-
-  def is_a_move?
-    !@content.include?("winner") and (@content.values.include?("o") or
-      @content.values.include?("x"))
   end
 
 end
--- spec/lib/game_recorder_spec.rb
+++ spec/lib/game_recorder_spec.rb
@@ -10,39 +10,21 @@ describe GameRecorder do
     it "writes to a specified stream in json and closes it" do
       greeting = {"greeting" => "hello"}
       GameRecorder.write_to_history(greeting,file)
-      file.should have_content(JSON.dump(greeting) + "\n")
+      file.should have_content(JSON.dump(greeting))
       file.close_called.should == true
     end
 
     it "doesn't write hashes with nil values in winner key" do
       winner = {"winner" => nil}
       GameRecorder.write_to_history(winner,file)
-      file.should_not have_content(JSON.dump(winner) + "\n")
+      file.should_not have_content(JSON.dump(winner))
     end
 
     it "writes hashes with non-nil values in winner key" do
       winner = {"winner" => "x"}
       GameRecorder.write_to_history(winner,file)
-      file.should have_content(JSON.dump(winner) + "\n")
+      file.should have_content(JSON.dump(winner))
     end 
-
-    it "puts moves in a moves array in the json file" do
-      move_one = {1 => "x"}
-      GameRecorder.write_to_history(move_one,file)
-      expected = {
-        "moves" => [move_one]
-      }
-      file.should have_content(JSON.dump(expected) + "\n")
-    end
-
-    it "appends moves to the exisitng moves data structure" do
-      GameRecorder.write_to_history({1 => "x"},file)
-      expected = {
-        "moves" => {"x" => [1], "o" => [3]}
-      }
-      GameRecorder.write_to_history({3 => "o"},file) 
-      file.should have_content(JSON.dump(expected))
-    end
   end
 end
 
