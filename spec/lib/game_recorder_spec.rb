410f4a2743ba453bd21b83011b4b94938f1e288e
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-13 21:41:35 -0500

    Factors implementation of data structure manipulation out of GameRecorder tests and puts it in GameState tests

--- spec/lib/game_recorder_spec.rb
+++ spec/lib/game_recorder_spec.rb
@@ -12,90 +12,26 @@ describe GameRecorder do
       GameRecorder.compute_file_contents(path).should == expected
     end
     
-    it "polls the contents of the file and overwrites existing structure" do
-      id = 1
-      first_move = {"token" => "x", "position" => 3}
-      next_move = {"token" => "o", "position" => 5}
-      old_structure = {
-        "games" => 
-          {
-          id =>
-          {
-            "moves" => [ first_move ],
-          }}}
-
-      new_structure = {
-        "games" => 
-          {
-          id =>
-          {
-            "moves" => [ first_move, next_move ],
-          }}}
-
+    it "overwrites what used to exist at path" do
+      id = "1"
       path = "foo"
-      File.stub(:read).with(path).and_return(old_structure.to_json)
-      File.should_receive(:write).with(path, JSON.pretty_generate(new_structure))
+      File.stub(:read).with(path).and_return("")
+      GameState.stub(:new_move).and_return(["new"])
+      File.should_receive(:write).with(path, JSON.pretty_generate(["new"]))
       GameRecorder.write_move(id, 5, "o", path)
     end
     
-    it "doesn't destroy old data" do
-      old_structure = {
-        "games" =>
-        {
-          "2" =>
-          {
-            "moves" => [{"token" => "x", "position" => 3}]
-          },
-
-          "3" =>
-          {
-            "moves" => [{"token" => "o", "position" => 5}]
-          }}}
-
-      new_structure = {
-        "games" =>
-        {
-          "2" =>
-          {
-            "moves" => [{"token" => "x", "position" => 3}]
-          },
-
-          "3" =>
-          {
-            "moves" => [{"token" => "o", "position" => 5},
-                        {"token" => "o", "position" => 4}]
-          }}}
-      path = "foo"
-      File.stub(:read).with(path).and_return(old_structure.to_json)
-      File.should_receive(:write).with(path, JSON.pretty_generate(new_structure))
-
-      GameRecorder.write_move(3, 4, "o", path)
-    end
   end
 
   describe ".write_winner" do
     
-    it "writes winner to exisitng structure" do
-      id = 8
-      move = {"token" => "x", "position" => 3}
-      old_structure = {"games" =>
-          {
-            id =>
-          {
-            "moves" => [ move ]
-          }}}
-
-      expected = {"games" =>
-          {
-          id =>
-          {
-            "moves" => [ move ],
-            "winner" => "x"
-          }}}
-
+    it "delegates to GameState" do
+      
       path = "??"
-      File.stub(:read).with(path).and_return(old_structure.to_json)
-      File.should_receive(:write).with(path,JSON.pretty_generate(expected))
+      winner = "x"
+      id = "3"
+      File.stub(:read).with(path).and_return("")
+      GameState.should_receive(:new_winner).with({"games"=>{}},winner,id).and_return({})
       GameRecorder.write_winner(id, "x", path)
     end
   end
---
+++ spec/lib/game_state_spec.rb
@@ -0,0 +1,86 @@
+require_relative '../../lib/game_state'
+
+describe GameState do
+
+  describe ".new_move" do
+    
+    it "creates a new game if it doesn't exist" do
+      id = "1"
+      move = 3
+      token = "x"
+      expected = {"games" =>
+        {
+          id => {
+            "moves" =>
+              [
+                "token" => token,
+                "position" => move
+              ]
+          }}}
+      GameState.new_move({"games" => {}}, token, move, id).should == expected
+    end
+
+    it "polls the data structure it gets and returns it with updated moves" do
+      id = "1"
+      first_move = {"token" => "x", "position" => 3}
+      next_move = {"token" => "o", "position" => 5}
+      old_structure = {
+        "games" => 
+          {
+          id =>
+          {
+            "moves" => [ first_move ],
+          }}}
+
+      new_structure = {
+        "games" => 
+          {
+          id =>
+          {
+            "moves" => [ first_move, next_move ],
+          }}}
+      GameState.new_move(old_structure, "o", 5, id).should == new_structure
+    end
+  end
+
+  describe ".write_winner" do
+    it "creates a new winner if it doesn't exist" do
+      id = "8"
+      move = {"token" => "x", "position" => 3}
+      old_structure = {"games" =>
+          {
+            id =>
+          {
+            "moves" => [ move ]
+          }}}
+
+      expected = {"games" =>
+          {
+          id =>
+          {
+            "moves" => [ move ],
+            "winner" => "x"
+          }}}
+      GameState.new_winner(old_structure, "x", id)
+    end
+    
+    it "overwrites the previous winner" do
+      id = "8"
+      move = {"token" => "x", "position" => 3}
+      old_structure = {"games" =>
+          {
+            id =>
+          {
+            "winner" => "HAHA I won!"
+          }}}
+
+      expected = {"games" =>
+          {
+          id =>
+          {
+            "winner" => "Not anymore!"
+          }}}
+      GameState.new_winner(old_structure, "Not anymore!", id).should == expected
+    end
+  end
+end
