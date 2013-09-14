13ced8b220d1d1f7d6aff88b1079a93dde27b494
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-13 20:29:16 -0500

    Moves GameState out of game_recorder.rb into its own file

--- lib/game_recorder.rb
+++ lib/game_recorder.rb
@@ -1,4 +1,5 @@
 require 'json'
+require_relative 'game_state'
 
 class GameRecorder
 
@@ -14,13 +15,13 @@ class GameRecorder
 
   def self.write_move(id, move, token, path="game_history.json")
     self.contents_of_file_at(path) do |contents|
-      GameState.after_new_move(contents, token, move, id)
+      GameState.new_move(contents, token, move, id)
     end
   end
 
   def self.write_winner(id,winner,path="game_history.json")
     self.contents_of_file_at(path) do |contents|
-      GameState.after_new_winner(contents,winner,id)
+      GameState.new_winner(contents,winner,id)
     end
   end
 
@@ -34,52 +35,4 @@ class GameRecorder
   
 end
 
-class GameState
 
-  def self.after_new_move(games, token, move, id)
-    to_add = {
-      "token" => token,
-      "position" => move
-    }
-
-    all_games = games["games"]
-
-    the_game = self.find_game(all_games, id)
-
-     if the_game["moves"]
-       move_list = the_game["moves"]
-     else
-       move_list = []
-       the_game["moves"] = move_list
-     end
-
-    new_move_list = move_list.concat([to_add])
-
-    games_after_update = {"games" => all_games}
-  end
-
-  def self.find_game(games, id)
-    if games[id.to_s]
-      the_game = games[id.to_s]
-    else
-      the_game = Hash.new
-      games[id.to_s] = the_game
-    end
-  end
-
-  def self.after_new_winner(games, winner, id)
-
-     to_add = {
-        "winner" => winner
-      }
-
-     all_games = games["games"]
-
-     the_game = self.find_game(all_games, id)
-
-     the_game["winner"] = winner
-     
-     {"games" => all_games}
-  end
-
-end
---
+++ lib/game_state.rb
@@ -0,0 +1,49 @@
+class GameState
+
+  def self.new_move(games, token, move, id)
+    to_add = {
+      "token" => token,
+      "position" => move
+    }
+
+    all_games = games["games"]
+
+    the_game = self.find_game(all_games, id)
+
+     if the_game["moves"]
+       move_list = the_game["moves"]
+     else
+       move_list = []
+       the_game["moves"] = move_list
+     end
+
+    new_move_list = move_list.concat([to_add])
+
+    games_after_update = {"games" => all_games}
+  end
+
+  def self.find_game(games, id)
+    if games[id.to_s]
+      the_game = games[id.to_s]
+    else
+      the_game = Hash.new
+      games[id.to_s] = the_game
+    end
+  end
+
+  def self.new_winner(games, winner, id)
+
+     to_add = {
+        "winner" => winner
+      }
+
+     all_games = games["games"]
+
+     the_game = self.find_game(all_games, id)
+
+     the_game["winner"] = winner
+     
+     {"games" => all_games}
+  end
+
+end
