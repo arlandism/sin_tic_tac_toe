0384a5ff17914da1e0ec3a7983739e3f1fd7d4c7
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-17 13:19:08 -0500

    Modifies cucumber test for game history feature to visit '/games' page

--- features/game_history.feature
+++ features/game_history.feature
@@ -4,4 +4,5 @@ Feature: Game History
 
   Scenario: Recording Game History
     Given I play a game
-    Then That game's history should be recorded
+    When I navigate to the game index
+    Then My game should appear
--- features/step_definitions/game_history_steps.rb
+++ features/step_definitions/game_history_steps.rb
@@ -1,5 +1,3 @@
-require 'json'
-
 Given(/^I play a game$/) do
     visit '/config'
     choose 'first_player_human'
@@ -9,11 +7,12 @@ Given(/^I play a game$/) do
     click_button '1'
 end
 
-Then(/^That game's history should be recorded$/) do
-   id = Capybara.current_session.driver.request.cookies["id"]
-   games = JSON.parse(File.read("game_history.json")) 
-   games["games"][id.to_s]["moves"][0]
-   first_move = games["games"][id.to_s]["moves"][0]
-   first_move["position"].should == 1
-   first_move["token"].should == "x" 
+When (/^I navigate to the game index$/) do
+  visit '/games'
 end
+
+Then(/^My game should appear$/) do
+   id = Capybara.current_session.driver.request.cookies["id"]
+   my_game = page.find_by_id(id)
+   my_game.should have_content("x moved to 1")
+end 
