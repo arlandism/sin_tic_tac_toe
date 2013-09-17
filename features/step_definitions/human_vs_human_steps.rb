a930d714225a800a2ac99badd55f85c245f84e95
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-17 13:27:18 -0500

    Driver issue with validation check in this test. Figuring out workaround WIP

--- features/step_definitions/human_vs_human_steps.rb
+++ features/step_definitions/human_vs_human_steps.rb
@@ -10,13 +10,9 @@ When(/^I select a Human as the first player and another human as the second$/) d
 end
 
 Then(/^We should be able to play against one another$/) do
-  click_button '1'
-  click_button '2'
-  click_button '3'
-  click_button '4'
-  click_button '5'
-  click_button '6'
-  click_button '8'
-  click_button '7'
-  click_button '9'
+  pending
+  #click_button '1'
+  #click_button '2'
+  #find_by_id(1)[:value].should == "x" 
+  #find_by_id(2)[:value].should == "o" 
 end
