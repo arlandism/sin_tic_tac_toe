fbb013b1ed86e4f6b60bec193f796284dfdb4a89
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-20 23:48:57 -0500

    Started work on the 'games/[0-9]+' feature

--- routes/games.rb
+++ routes/games.rb
@@ -1,4 +1,5 @@
 require 'haml'
+require 'json'
 
 require_relative '../lib/presenters/id_presenter'
 
@@ -8,8 +9,13 @@ class TTTDuet < Sinatra::Base
       haml :games
   end
 
-  get %r{/games/[0-9]+} do
-  
+  get %r{/games/([0-9]+)} do
+    game_id = params[:captures].first
+    games = History.retrieve_or_create(TTTDuet.settings.history_path)
+    queried_game = games["games"][game_id]
+    response.set_cookie("id", game_id)
+    response.set_cookie("moves", JSON.dump(queried_game["moves"]))
+    response.set_cookie("winner", queried_game["winner"])
   end
 
 end
--- spec/routes/games_spec.rb
+++ spec/routes/games_spec.rb
@@ -1,4 +1,5 @@
 require 'rack/test'
+require 'json'
 
 require_relative '../../app'
 
@@ -13,8 +14,27 @@ describe TTTDuet do
     
     it "takes me to the page of the game" do
       get '/games/22' 
-      last_response.status.should == 200 
+      last_request.url.should == "http://example.org/games/22" 
     end 
+
+    it "polls History for the queried game" do
+
+      game = {
+        "moves" => [{"position" => 2, "token" => "x"}],
+        "winner" => "me!"
+        }
+      game_id = "22"
+
+      History.should_receive(:retrieve_or_create).
+        with(TTTDuet.settings.history_path).
+        and_return({"games" => {game_id => game}})
+
+      get '/games/22' 
+
+      rack_mock_session.cookie_jar["id"].should == "22"
+      rack_mock_session.cookie_jar["moves"].should == JSON.dump(game["moves"])
+      rack_mock_session.cookie_jar["winner"].should == "me!"
+    end
   end
 
 end
