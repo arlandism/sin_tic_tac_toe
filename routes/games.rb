bbb56d52c14f6191ebe333ae009b7b4837519ce3
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-21 13:14:12 -0500

    Code deletion in routes/games.rb

--- routes/games.rb
+++ routes/games.rb
@@ -12,10 +12,6 @@ class TTTDuet < Sinatra::Base
   get %r{/games/([0-9]+)} do
     game_id = params[:captures].first
     games = History.retrieve_or_create(TTTDuet.settings.history_path)
-    queried_game = games["games"][game_id]
-    response.set_cookie("id", game_id)
-    response.set_cookie("moves", JSON.dump(queried_game["moves"]))
-    response.set_cookie("winner", queried_game["winner"])
   end
 
 end
