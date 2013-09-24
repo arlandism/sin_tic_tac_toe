695d031197053930612a7b1fd60574ab85f9eb80
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-24 14:35:23 -0500

    Adds FileHistoryReaderWriter

--- lib/file_history_reader_writer.rb
+++ lib/file_history_reader_writer.rb
@@ -8,4 +8,11 @@ class FileIO
     File.write(path, JSON.pretty_generate(contents))
   end
 
+  def self.open_and_write_to(path)
+    file_contents = self.retrieve_or_create(path)
+
+    new_contents = yield file_contents
+
+    writer.write(path, new_contents)
+  end
 end
