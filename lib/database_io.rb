8f894a053d90866b01913dbcb08efd6b66212b13
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-26 16:56:25 -0500

    Adds id back to Move model to keep moves unique

--- lib/database_io.rb
+++ lib/database_io.rb
@@ -39,6 +39,7 @@ class DatabaseIO
 
   def self.create_move(id, position, token)
     Move.create(
+      :id => id,
       :position => position,
       :token => token,
       :game_id => id
--- lib/models/move.rb
+++ lib/models/move.rb
@@ -3,9 +3,10 @@ require 'data_mapper'
 class Move
   include DataMapper::Resource
 
+  property :id, Serial, :key => true
   property :position, Integer
   property :token, String
 
-  belongs_to :game, :key => true
+  belongs_to :game
 
 end
