abcc48aaa92f652f137a6f3e2954b322545badac
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-29 13:00:31 -0500

    Switches out FileHistory for HistoryAccessor

--- routes/move.rb
+++ routes/move.rb
@@ -1,11 +1,12 @@
 require_relative '../lib/file_history'
 require_relative '../lib/game_information'
 require_relative '../lib/next_player'
+require_relative '../lib/history_accessor'
 
 class TTTDuet < Sinatra::Base
 
   post '/move' do
-    id = cookies["id"] || FileHistory.next_id(settings.history_path)
+    id = cookies["id"] || HistoryAccessor.next_id(settings.history_path)
     response.set_cookie("id",id)
     token_one = token(cookies)
     place_move_on_board(first_player_move,token_one)
@@ -20,14 +21,14 @@ class TTTDuet < Sinatra::Base
   def place_move_on_board(move,token)
     response.set_cookie(move,token)
     id = cookies["id"].to_i
-    FileHistory.write_move(id, move.to_i, token, settings.history_path)
+    HistoryAccessor.write_move(id, move.to_i, token, settings.history_path)
   end
  
  def place_winner_on_board
     winner = GameInformation.new(cookies).winner_on_board
     response.set_cookie("winner",winner)
     id = cookies["id"].to_i
-    FileHistory.write_winner(id,winner, settings.history_path)
+    HistoryAccessor.write_winner(id,winner, settings.history_path)
   end
 
   def first_player_move 
