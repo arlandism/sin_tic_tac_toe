6da25a11c9aea76e71e95880461218279d143b05
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-29 12:57:06 -0500

    Adds DataMapper setup in app

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/cookies'
 require 'sinatra/config_file'
+require 'yaml'
 
 class TTTDuet < Sinatra::Base
   register Sinatra::ConfigFile
@@ -13,5 +14,9 @@ end
 require_relative 'routes/init'
 
 if __FILE__ == $0 
+  db_config = YAML.load_file("database.yaml")["development"]
+  DataMapper.setup(:default, "#{db_config["adapter"]}://#{db_config["user"]}: #{db_config["password"]} @#{db_config["host"]}/#{db_config["database"]}")
+  DataMapper.finalize
+  DataMapper.auto_migrate!
   TTTDuet.run!
 end
