d1d607a7b71ce53a688f469504ae8bbebf02981f
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-09-29 14:58:40 -0500

    Changes FileHistory to HistoryAccessor in routes/index

--- routes/index.rb
+++ routes/index.rb
@@ -1,13 +1,14 @@
 require 'haml'
 
 require_relative '../lib/cpu_move'
+require_relative '../lib/history_accessor'
 require_relative '../lib/presenters/button_presenter'
 require_relative '../lib/presenters/winner_presenter'
 
 class TTTDuet < Sinatra::Base
 
   get '/' do
-    id = cookies["id"] || FileHistory.next_id(settings.history_path)
+    id = cookies["id"] || HistoryAccessor.next_id(settings.history_path)
     response.set_cookie("id", id)
     if CpuMove.should_place(cookies.to_hash) 
       place_move_on_board(next_player_move, token(cookies))
